2c6a4635bf7ae85765922a95c809cccf3257d1c4
Jim DeSelms <jdeselms@vistaprint.com>
2020-10-28 11:51:19 -0400

    Separate instructinos from the vm

---
+++ src/vm/instructions.js
@@ -0,0 +1,28 @@
+class Instructions {
+    static halt(state) {
+        state.isHalted = true
+    }
+
+    static ldIntegerToRegister(state, register) {
+        const value = state.memory[state.ip++]
+        state[register] = value
+    }
+
+    static nop() {
+    }
+}
+
+const OPCODES = {
+    // NOP
+    0b00000000: { code: Instructions.nop, cycles: 1 },
+
+    // HALT
+    0b01110110: { code: Instructions.halt, cycles: 1 },
+    
+    // LD r, n
+    0b00111110: { code: state => Instructions.ldIntegerToRegister(state, "A"), cycles: 2 }
+}
+
+module.exports = {
+    OPCODES
+}
--- src/vm/vm.js
+++ src/vm/vm.js
@@ -1,27 +1,4 @@
-class Instructions {
-    static halt(state) {
-        state.isHalted = true
-    }
-
-    static ldIntegerToRegister(state, register) {
-        const value = state.memory[state.ip++]
-        state[register] = value
-    }
-
-    static nop() {
-    }
-}
-
-const OPCODES = {
-    // NOP
-    0b00000000: { code: Instructions.nop, cycles: 1 },
-
-    // HALT
-    0b01110110: { code: Instructions.halt, cycles: 1 },
-    
-    // LD r, n
-    0b00111110: { code: state => Instructions.ldIntegerToRegister(state, "A"), cycles: 2 }
-}
+const { OPCODES } = require('./instructions')
 
 class Vm {
     constructor({ size, initialImage }) {
